11fae3a7f7c22ba3d4284cbfc21a7d77933308d2
LAPEK\Dawid <wox0@interia.eu>
2017-03-03 16:54:37 +0100

    Added jump.

--- Assets/Scipts/PlayerController.cs
+++ Assets/Scipts/PlayerController.cs
@@ -4,22 +4,33 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-    public int speed;
+    public int Speed;
+    public int JumpHeight;
 
-    private Rigidbody rb;
+    private Rigidbody _rb;
 
 
-	void Awake () {
-        rb = GetComponent<Rigidbody>();
+	public void Awake () {
+        _rb = GetComponent<Rigidbody>();
 	}
-	
-	
-	void FixedUpdate () {
+
+
+    public void FixedUpdate () {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        
 
-        rb.AddForce(movement * speed);
-	}
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        
+
+        _rb.AddForce(movement * Speed);
+	    if (Input.GetKey("r"))
+	    {
+            _rb.AddForce(Vector3.up * JumpHeight);
+            //Debug.Log("R Clicked");
+        }
+
+
+    }
 }
